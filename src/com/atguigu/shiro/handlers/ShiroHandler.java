1854dd656fdb782c7fb35339f5dd9f5493c220bf
AnshayM <905928827@qq.com>
2018-10-31 11:51:18 +0800

    对密码进行md5加密

--- src/com/atguigu/shiro/handlers/ShiroHandler.java
+++ src/com/atguigu/shiro/handlers/ShiroHandler.java
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ShiroHandler {
 
 	@RequestMapping("/login")
-	public String login( String username, String password) {
+	public String login(String username, String password) {
 		Subject currentUser = SecurityUtils.getSubject();
-		System.out.println("ddddd");
 		if (!currentUser.isAuthenticated()) {
 			// 把用户名和密码封装为UsernamePasswordToken对象
 			UsernamePasswordToken token = new UsernamePasswordToken(username, password);
@@ -28,12 +27,29 @@ public class ShiroHandler {
 				System.out.println("登陆失败:" + ae.getMessage());
 			}
 		}
-		return "redirect:/login.jsp";
-
+		return "redirect:/list.jsp";
 	}
-	@RequestMapping("/hello")
-	public String hello() {
-		return "list";
+
+	/**
+	 * 登出（还没有做清除缓存操作）
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/logout")
+	public String logout() {
+
+		Subject currentUser = SecurityUtils.getSubject();
+		if (!currentUser.isAuthenticated()) {
+			// 把用户名和密码封装为UsernamePasswordToken对象
+			// UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+			try {
+				// 执行登陆
+				currentUser.logout();
+			} catch (AuthenticationException ae) {
+				System.out.println("登出失败:" + ae.getMessage());
+			}
+		}
+		return "redirect:/login.jsp";
 	}
 
 }
--- src/com/atguigu/shiro/realms/ShiroRealm.java
+++ src/com/atguigu/shiro/realms/ShiroRealm.java
@@ -7,8 +7,10 @@ import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthenticatingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
 
 public class ShiroRealm extends AuthenticatingRealm {
 
@@ -38,14 +40,28 @@ public class ShiroRealm extends AuthenticatingRealm {
 		// 1). principal: 认证的实体信息. 可以是 username, 也可以是数据表对应的用户的实体类对象.
 		Object principal = username;
 		// 2). credentials: 密码.
-		Object credentials = "123456"; // "fc1709d0a95a6be30bc5926fdb7f22f4";
+		Object credentials = "fc1709d0a95a6be30bc5926fdb7f22f4";
 		// 3). realmName: 当前 realm 对象的 name. 调用父类的 getName() 方法即可
 		String realmName = getName();
-		System.out.println(realmName);
 
 		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal, credentials, realmName);
 
 		return info;
+	}
+
+	/**
+	 * 测试加密1024次后结果： 不加盐--fc1709d0a95a6be30bc5926fdb7f22f4
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String hashAlgorithmName = "MD5";
+		Object credentials = "123456";
+		Object salt = null;
+		int hashIterations = 1024;
+
+		Object result = new SimpleHash(hashAlgorithmName, credentials, salt, hashIterations);
+		System.out.println(result);
 	}
 
 }
